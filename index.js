b197ee62790c568e2a2f5ff7fb2ee577e3a26e38
Emmlass <enaibreemma@gmail.com>
2025-11-01 16:04:47 +0100

    first commit for part3

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+}
+);
+
+app.get('/api/notes/:id',(req,res)=>{
+  const id = req.params.id;
+  const note = notes.find(note=> note.id === id);
+  res.json(note);
+});
+
+const PORT = 300;
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
+
